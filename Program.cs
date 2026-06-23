d129751d80e4363a4b5f1a4fcf59d273af107865
anonymouse-potato-chips <alexslybird@gmail.com>
2026-06-23 20:24:59 +0700

    Update Program.cs line 4 to display Hello from 1st-repo

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapGet("/", () => Results.Ok("Hello from 1st-repo"));
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
-app.MapGet("/", () => Results.File("index.html"));
 
 app.Run();
